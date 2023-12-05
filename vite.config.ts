07ad08a2c23210a62f158713dd86238dd3306492
zato4en <lavrenovmihail2103@gmail.com>
2023-12-05 19:29:28 +0300

    built but deletion not working

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontendrip",
+  base: "/frontendrip/",
   server: {
     proxy: {
       '/api': 'http://localhost:8888',
