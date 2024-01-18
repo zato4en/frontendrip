c4cab46a7e4799f00192a7ba4893e64520050739
zato4en <lavrenovmihail2103@gmail.com>
2024-01-18 18:46:09 +0300

    lab4 corrected

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://192.168.50.209:8888',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
